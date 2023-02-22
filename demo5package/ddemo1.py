601642d493cd1838c889de98523eeb8ae94dc7b0
Jui Shah <Jui.Shah@einfochips.com>
2023-02-22 11:52:48 +0530

    add loops code

--- demo5package/ddemo1.py
+++ demo5package/ddemo1.py
@@ -1,6 +1,17 @@
-def printhello():
-    print("hello from demo1.py")
+#def printhello():
+ #   print("hello from demo1.py")
 
 
-class chromedemo :
-    pass
+#class chromedemo :
+ #   pass
+
+for a in range(1,10):
+    print(a)
+for a in range(1,10,2):
+    print(a)
+
+numbers=[45,98,75,65,24,88,74,56,75]
+for n in range(0,len(numbers)):
+  if(numbers[n]<50):
+   print(numbers[n])
+
